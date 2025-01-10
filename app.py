d7ce35ef249f6362eb5b348f9593de010d56b852
balakrishnasurya <90981476+balakrishnasurya@users.noreply.github.com>
2025-01-10 11:23:35 +0530

    Commented and changed names

--- app.py
+++ app.py
@@ -182,5 +182,5 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000) 
+#if __name__ == "__main__":
+#    app.run(debug=True, host="0.0.0.0", port=5000) 
